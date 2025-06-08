b84843ccb92bc2ea38a0e18b684b3719e9ef4cb7
Vedant <borodevedant4@gmail.com>
2025-06-08 18:57:13 +0530

    feat(course): add funtionality to /preview and /purchase endpoint

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,19 +1,46 @@
 const express = require("express");
 const {Router} = express;
 // const Router = express.Router();
-const Course = require("../models/course.model.js");
+const { Course } = require("../models/course.model.js");
+const { userAuthMiddleware } = require("../middlewares/user.auth.middleware.js");
+const z = require("zod");
+const { Purchase } = require("../models/purchases.model.js");
 
 const courseRouter = Router();
 
-courseRouter.post("/purchase", (req, res) => {
-    res.json({
-        message: "Purchase endpoint"
-    })
+const courseIdZodSchema = z.object({
+    courseId: z.string()
+})
+
+courseRouter.post("/purchase", userAuthMiddleware , async (req, res) => {
+
+    const userId = req.userId
+    const parsedData = courseIdZodSchema.safeParse(req.body)
+
+    //verify the payment recieved
+
+    try {
+        await Purchase.create({
+            userId: userId,
+            courseId: parsedData.data.courseId
+        })
+        
+        res.json({
+            message: "You have brought the course"
+        })
+    } catch (error) {
+        res.json({
+            message: "course purchase failed"
+        })
+    }
 });
 
-courseRouter.get("/preview", (req, res) => {
+courseRouter.get("/preview", async (req, res) => {
+
+    const courses = await Course.find({})
+
     res.json({
-        message: "Preview endpoint"
+        courses
     })
 });
 
